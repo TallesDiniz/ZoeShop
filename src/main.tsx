a50bb7e43fb390d7705db797be022752971ce4c6
Talles Diniz <tallessdiniz@gmail.com>
2026-04-04 02:30:32 -0300

    feat: inclusão do react-hot-toast ao projeto

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,37 @@ import './index.css'
 import { router } from './App'
 import { RouterProvider } from 'react-router-dom'
 import { CartProvider } from './context/CartProvider'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
+      <Toaster 
+      position="top-right" 
+      toastOptions={{
+        duration: 1000,
+        style: {
+          background: '#5C3A1E',
+          color: '#FDFAF6',
+          fontFamily: '"DM Sans", sans-serif',
+          borderRadius: '100px',
+          padding: '12px 20px',
+          fontSize: '14px',
+        },
+        success: {
+          iconTheme: {
+            primary: '#C49A6C',
+            secondary: '#FDFAF6',
+          },
+        },
+        error: {
+          iconTheme: {
+            primary: '#EF4444',
+            secondary: '#FDFAF6',
+          },
+        },
+      }}
+      />
       <RouterProvider router={router} />
     </CartProvider>
   </StrictMode>,
